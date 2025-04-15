8ecd8544a8c837035b5997349d939e172ec6b8a9
Brian Carrier <carrier@sleuthkit.org>
2025-04-15 15:14:43 -0400

    .

--- release/VS2019_cygwin.bat
+++ release/VS2019_cygwin.bat
@@ -3,7 +3,7 @@
 REM Launch a Cygwin shell with the needed Visual Studio 2019 environment variables set.
 REM Used to run the automated build / release scripts that are written in Perl/Python.
 
-REM CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 C:
 chdir C:\cygwin64\bin
